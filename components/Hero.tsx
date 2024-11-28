f55b2532cf9eac292f6fe5f2d28bc0114d4a9a93
Edward <edw.chydi@gmail.com>
2024-11-28 19:52:07 +0900

    navbar mobile hamburger fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
           Our Summer Collection
         </p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 max-sm:z-0">
             The New Arrival
           </span>
           <br />
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
-      <nav className="flex justify-between items-center max-container sm:items-start">
+      <nav className="flex justify-between items-center max-container max-sm:items-start">
         <Link href="/">
           <Image src={headerLogo} alt="Logo" width={130} height={29}></Image>
         </Link>
